a24c019c024b0c3fa0a100f09a722bef0cd25381
matheus roberto albuquerque de souza <malbuquerquedesouza@gmail.com>
2020-07-15 20:29:45 -0300

    churrasco

---
+++ script.js
@@ -0,0 +1,53 @@
+// Carne - 400 gr por pessoa + de 6 horas - 650
+// Cerveja  1200 ml por pessoa + 6 horas - 2000ml
+// Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500ml
+
+// Crianças valem 0,5
+
+let inputadultos = document.getElementById("adultos");
+let inpuCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado")
+
+function calcular(){
+    console.log("calculando")
+
+    let adultos = inputadultos.value;
+    let criancas = inpuCriancas.value;
+    let duracao = inputDuracao.value;
+ 
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2  * criancas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos ;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2  * criancas);
+    console.log(qdtTotalCarne,qdtTotalCerveja,qdtTotalBebidas)
+
+    resultado.innerHTML = `<p>${qdtTotalCarne / 1000}g de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} Latas de Cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas / 2000)} Pet's 2L de Bebidas</p>`
+}
+
+function carnePP(duracao){
+    if (duracao >= 6){
+        return  650;
+    }else{
+        return 400;
+    }
+}
+
+function cervejaPP(duracao){
+    if (duracao >= 6){
+        return  2000;
+    }else{
+        return 1200;
+    }
+}
+function bebidasPP(duracao){
+    if (duracao >= 6){
+        return  1500;
+    }else{
+        return 1000;
+    }
+}
+
+
